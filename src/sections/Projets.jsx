cdd0d31dcc91251053d715cfc48486b1b51232dd
nanadocte <206342507+nanadocte@users.noreply.github.com>
2026-02-22 21:41:15 +0100

    Implement filter function to Projet section

--- src/sections/Projets.jsx
+++ src/sections/Projets.jsx
@@ -1,37 +1,52 @@
 import SectionTitle from '../components/SectionTitle';
 import projets from '../data/projets.json';
 import ProjetCard from '../components/ProjetCard';
+import { useState } from 'react';
 
 function Projets() {
+  const [buttonActif, setButtonActif] = useState('Tous');
   const uniqueStack = [...new Set(projets.flatMap((projet) => projet.stack))];
-  console.log(uniqueStack);
   return (
     <section className="px-76 p-32 bg-[#020617] flex flex-col gap-3">
       <SectionTitle sectionName="03. Projets" contenu="Mes réalisations" />
       <ul className="flex gap-5 text-sm mb-6">
-        <li className="hover:bg-gradient-to-r hover:from-indigo-800  hover:to-purple-600  bg-slate-800 py-1 px-3 rounded-2xl text-slate-400">
+        <li
+          className={`hover:bg-gradient-to-r hover:from-indigo-800  hover:to-purple-600  bg-slate-800 
+        py-1 px-3 rounded-2xl text-slate-400
+        ${buttonActif === 'Tous' ? 'bg-gradient-to-r from-indigo-800 to-purple-600' : 'bg-slate-800'}`}
+          onClick={() => setButtonActif('Tous')}
+        >
           Tous
         </li>
         {uniqueStack.map((projet) => (
           <li
-            className="hover:bg-gradient-to-r hover:from-indigo-800  hover:to-purple-600 bg-slate-800 py-1 px-3 rounded-2xl text-slate-400"
+            className={`hover:bg-gradient-to-r hover:from-indigo-800  hover:to-purple-600 bg-slate-800 
+            py-1 px-3 rounded-2xl text-slate-400
+            ${buttonActif === projet ? 'bg-gradient-to-r from-indigo-800 to-purple-600' : 'bg-slate-800'}
+            `}
+            onClick={() => setButtonActif(projet)}
             key={projet}
           >
-            <button>{projet}</button>
+            {projet}
           </li>
         ))}
       </ul>
       <div className="flex gap-12 justify-center">
-        {projets.map((projet) => (
-          <ProjetCard
-            key={projet.id}
-            img={projet.img}
-            titre={projet.titre}
-            description={projet.description}
-            stack={projet.stack}
-            github={projet.github}
-          />
-        ))}
+        {projets
+          .filter(
+            (projet) =>
+              buttonActif === 'Tous' || projet.stack.includes(buttonActif)
+          )
+          .map((projet) => (
+            <ProjetCard
+              key={projet.id}
+              img={projet.img}
+              titre={projet.titre}
+              description={projet.description}
+              stack={projet.stack}
+              github={projet.github}
+            />
+          ))}
       </div>
     </section>
   );
